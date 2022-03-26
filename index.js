3b0b945471c3a802b723ee1d55725d7eed5d641f
Saikat Mukherjee <75084468+saikatmu31@users.noreply.github.com>
2022-03-26 20:41:13 +0530

    DB Connection

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+require("dotenv").config();
+require("./config/database").connect();
 
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/api/home", function (req, res) {
+  res.json({
+    message: "Hi Everyone Welcome To Website",
+  });
+});
 
-app.get('/api/home', function (req, res) {
+app.get("/api", function (req, res) {
+  try {
     res.json({
-        message: 'Hi Everyone Welcome To Website'
-    })
-})
+      message: "Hi Abir Pal This Is Succcess Route",
+      success: true,
+    });
+  } catch (error) {
+    res.json({
+      message: "Hi Abir Pal",
+      success: false,
+      errMessage: error.message,
+    });
+  }
+});
 
-app.get('/api', function (req, res) {
-    try {
-        res.json({
-            message: 'Hi Abir Pal This Is Succcess Route',
-            success: true
-        })
-    } catch (error) {
-        res.json({
-            message: 'Hi Abir Pal',
-            success: false,
-            errMessage: error.message
-        })
-    }
-
-})
-
-const PORT = process.env.PORT || 4000
-app.listen(4000, ()=>{
-    console.log(`App connected to the server at Port ${PORT}`);
-}) 
+const PORT = process.env.PORT || 4000;
+app.listen(4000, () => {
+  console.log(`App connected to the server at Port ${PORT}`);
+});
