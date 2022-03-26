120a4957f16f9dfb02f9b630de4804ca4db2aec8
Abir Pal <abirpal282002@gmail.com>
2022-03-26 17:49:15 +0530

    Added Base Layout

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,38 +1,34 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import "./App.css"
-import Navabar from './Components/Navbar'
-
+import Home from './Pages/Home'
 
 const App = () => {
-  const [message , setMessage] = useState("")
-  const [msg , setMsg] = useState("")
+  // const [message , setMessage] = useState("")
+  // const [msg , setMsg] = useState("")
 
 
-  const fetchData = async () => {
-    const {data} = await axios.get("/api/home");
-    console.log("Parsed Data : ", data);
-    setMessage(data.message)
+  // const fetchData = async () => {
+  //   const {data} = await axios.get("/api/home");
+  //   console.log("Parsed Data : ", data);
+  //   setMessage(data.message)
 
-    const parsedData = await axios.get("/api")
-    console.log("Data : ", parsedData.data);
-    if(parsedData.data.success){
-      setMsg(parsedData.data.message)
-    }else{
-      setMsg(parsedData.data.message)
-    }
-  }
+  //   const parsedData = await axios.get("/api")
+  //   console.log("Data : ", parsedData.data);
+  //   if(parsedData.data.success){
+  //     setMsg(parsedData.data.message)
+  //   }else{
+  //     setMsg(parsedData.data.message)
+  //   }
+  // }
 
-  useEffect(() => {
-    fetchData();
-  }, [])
+  // useEffect(() => {
+  //   fetchData();
+  // }, [])
   
   return (
     <>
-      <Navabar />
-      <h1>Hi Everyone!</h1>
-      <p>{message}</p>
-      <h2>{msg}</h2>
+      <Home />
     </>
   )
 }
---
+++ Client/src/Base.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const Base = ({children}) => {
+  return (
+    <>
+        <Navbar/>
+        <div>
+            {children}
+        </div>
+        <footer>
+          This is the footer component
+        </footer>
+    </>
+  )
+}
+
+export default Base
