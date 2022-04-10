277451c1055acb1affd089a9641b642bdc24a8ef
Abir Pal <abirpal282002@gmail.com>
2022-04-10 15:19:12 +0530

    Fetching the data from Backend and Diplaying the docs

--- Client/src/Pages/Alldocuments.jsx
+++ Client/src/Pages/Alldocuments.jsx
@@ -1,35 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation, Link } from "react-router-dom";
 import Base from "../Base";
+import axios from "axios";
 
 const Alldocuments = () => {
   let navigate = useNavigate();
   let loation = useLocation();
-  const [title, setTitle] = useState("");
+  const [tag, setTag] = useState("");
 
   async function tokenCheker() {
-    //   console.log(loation.pathname);
     const authToken = localStorage.getItem("token");
     if (!authToken) {
       navigate("/");
     }
   }
 
-  useEffect(() => {
+  async function getDocuments() {
     let pathName = loation.pathname;
     let pathArray = pathName.split("/");
     console.log(pathArray);
-    setTitle(pathArray[2].replaceAll("%20", " "));
+    setTag(pathArray[2].replaceAll("%20", " "));
+
+    console.log("Fetch Data");
+    const authToken = localStorage.getItem("token");
+    const { data } = await axios.get(`/api/v1/getdocs/${pathArray[2].replaceAll("%20", " ")}`, {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${authToken}` },
+    });
+    console.log("Doc Data : ", data);
+  }
+
+  useEffect(() => {
+    getDocuments();
     tokenCheker();
   }, []);
 
   return (
     <Base>
-      <h1>All Document Related To {title}</h1>
-
+      <h1>All Document Related To {tag}</h1>
       Here all the documents of specific tag will be displayed
       <form>
-        <Link to={`/docs/${title}/createDocument`}>
+        <Link to={`/docs/${tag}/createDocument`}>
           <button className="btn">Create Document</button>
         </Link>
       </form>
