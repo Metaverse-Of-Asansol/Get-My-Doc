66af52f542ef65e4a8c306c4fcbab9bf1f0b6398
Abir Pal <abirpal282002@gmail.com>
2022-04-10 21:01:41 +0530

    Added View Page

--- Client/src/Pages/Viewdata.jsx
+++ Client/src/Pages/Viewdata.jsx
@@ -1,12 +1,42 @@
-import React, {useEffect} from 'react'
-import Base from '../Base'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Base from "../Base";
+import axios from "axios";
 
 const Viewdata = () => {
+  const [Userdoc, setUserdoc] = useState({});
+  let loation = useLocation();
+  const getData = async () => {
+    console.log(location.pathname.split("/")[2]);
+    const authToken = localStorage.getItem("token");
+    const { data } = await axios.get(
+      `/api/v1/getdocument/${location.pathname.split("/")[2]}`,
+      {
+        withCredentials: true,
+        headers: { Authorization: `Bearer ${authToken}` },
+      }
+    );
+    console.log("Data Parsed : ", data);
+    setUserdoc(data.docs);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Base>
-        <h1>View Data Page</h1>
+      {console.log(Userdoc)}
+      <h1>{Userdoc.docTitle}</h1>
+      <h2>{Userdoc.docId}</h2>
+      <h1>
+        <code>{Userdoc.docTags}</code>
+      </h1>
+      <div>
+        <p>{Userdoc.additionalInfo}</p>
+      </div>
+      <img src={Userdoc.docLink} alt="" />
     </Base>
-  )
-}
+  );
+};
 
-export default Viewdata
+export default Viewdata;
